c18b9d720e25733c04487a290da9dbc1776aec14
Samuel Albrecht <elsamuko@gmail.com>
2019-05-25 13:23:07 +0200

    chg: initialize boyer moore horspool and knuth morris pratt

--- src/string_search.cpp
+++ src/string_search.cpp
@@ -26,8 +26,11 @@ int main() {
     size_t pos = 0;
     void* ptr = nullptr;
     std::string::iterator it;
+    boost::algorithm::boyer_moore_horspool bmh( term.begin(), term.end() );
+    boost::algorithm::knuth_morris_pratt kmp( term.begin(), term.end() );
 
     printf( "%s with %s\n", BOOST_PLATFORM, BOOST_COMPILER );
+    printf( "~~~~\n" );
 
 #if !WIN32
     boost::int_least64_t t_memmem = timed1000( "memmem", [&text, &term, &ptr] {
@@ -43,14 +46,15 @@ int main() {
         ptr = ( void* )strstr( text.data(), term.data() );
     } );
 
-    boost::int_least64_t t_BMH = timed1000( "boyer_moore_horspool_search", [&text, &term, &it] {
-        it = boost::algorithm::boyer_moore_horspool_search( text.begin(), text.end(), term.begin(), term.end() ).first;
+    boost::int_least64_t t_BMH = timed1000( "boyer_moore_horspool_search", [&text, &term, &it, &bmh] {
+        it = bmh( text.begin(), text.end() ).first;
     } );
 
-    boost::int_least64_t t_KMP = timed1000( "knuth_morris_pratt_search", [&text, &term, &it] {
-        it = boost::algorithm::knuth_morris_pratt_search( text.begin(), text.end(), term.begin(), term.end() ).first;
+    boost::int_least64_t t_KMP = timed1000( "knuth_morris_pratt_search", [&text, &term, &it, &kmp] {
+        it = kmp( text.begin(), text.end() ).first;
     } );
 
+    printf( "~~~~\n" );
     printf( "\n" );
 
     return 0;
